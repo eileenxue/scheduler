cfcb32d90db46b74d1fc18c8046dc4ed3134d220
Eileen <xue.eileen@gmail.com>
2021-10-26 19:32:17 -0400

    Update button component and related tests

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,23 @@
 import React from "react";
+import classNames from "classnames";
 
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+  // FIRST WAY
+  // let buttonClass = "button";
+  // if (props.confirm) {
+  //   buttonClass += " button--confirm";
+  // } 
+  // if (props.danger) {
+  //   buttonClass += " button--danger";
+  // }
+
+  // BETTER WAY
+  const buttonClass = classNames("button", {
+    "button--confirm": props.confirm,
+    "button--danger": props.danger
+  })
+
+  return <button className={buttonClass} onClick={props.onClick} disabled={props.disabled}>{props.children}</button>;
 }
