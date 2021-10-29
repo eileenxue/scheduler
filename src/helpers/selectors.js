3422300fd6bba5a6de3b3a405e9acf9d7c90e3f0
Eileen <xue.eileen@gmail.com>
2021-10-28 23:39:35 -0400

    Add selector helper functions

---
+++ src/helpers/selectors.js
@@ -0,0 +1,20 @@
+export function getAppointmentsForDay(state, day) {
+  // 1. Find the object in state.days array that matches the name of day
+  const filterDay = state.days.filter(match => match.name === day)[0];
+  // console.log(filterDay.appointments)
+  // console.log(filterDay[0].appointments.map((id) => state.appointments[id]))
+  
+  // console.log(state.appointments)
+  // console.log(Object.keys(state.appointments))
+  // for (let appointmentID of Object.keys(state.appointments)){
+    //   console.log("ApptID:"+ appointmentID)
+    // }
+    
+  // 2. Iterate through the appointments and return the value
+  // 3. Validation where it's empty
+  if (!state.days.length || !filterDay) {
+    return [];
+  } else{
+    return filterDay.appointments.map((id) => state.appointments[id])
+  }
+}
