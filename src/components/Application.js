65e39c580145597e84c49abeed2a4a9d9f905fca
Eileen <xue.eileen@gmail.com>
2021-10-29 00:25:56 -0400

    Update and manage states for days

--- src/components/Application.js
+++ src/components/Application.js
@@ -45,12 +45,21 @@ const appointments = [
 
 export default function Application(props) {
 
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
+  // const [day, setDay] = useState("Monday");
+  // const [days, setDays] = useState([]);
+
+  const setDay = day => setState({ ...state, day });
+  const setDays = (days) => setState(prev => ({ ...prev, days }));
+
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    // you may put the line below, but will have to remove/comment hardcoded appointments variable
+    appointments: {}
+  });
 
   useEffect(() => {
     axios.get("http://localhost:8001/api/days").then(response => {
-      // console.log(response.data);
       setDays([...response.data]);
     })
   }, [])
@@ -66,8 +75,8 @@ export default function Application(props) {
       <hr className="sidebar__separator sidebar--centered" />
       <nav className="sidebar__menu">
         <DayList 
-          days={days} 
-          value={day} 
+          days={state.days} 
+          value={state.day} 
           onChange={setDay}
         />
       </nav>
